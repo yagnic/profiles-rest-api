1c6a3e50063a824bbd1d0bcdfe915271bb0fcbbe
yagnic <30473493+yagnic@users.noreply.github.com>
2021-05-11 17:50:37 +0530

    authentication feed api complete with logged in user restriction

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,10 +8,12 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello_viewset', views.HelloViewSet, base_name='hello_viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 
 ]
